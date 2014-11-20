dade43d5582da1ba580121a6b6663a98e8da03e2
songxh <tsingheng@163.com>
2014-11-20 12:19:26 +0800

    create parent pom

--- shop-base/src/main/java/net/shangtech/shop/product/service/impl/CategoryService.java
+++ shop-base/src/main/java/net/shangtech/shop/product/service/impl/CategoryService.java
@@ -1,15 +1,26 @@
 package net.shangtech.shop.product.service.impl;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import net.shangtech.framework.service.BaseService;
+import net.shangtech.shop.product.dao.ICategoryDao;
 import net.shangtech.shop.product.entity.Category;
 import net.shangtech.shop.product.service.ICategoryService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
 public class CategoryService extends BaseService<Category> implements ICategoryService {
+
+	@Autowired private ICategoryDao dao;
+	
+	@Override
+	public List<Category> findByParentId(Long parentId) {
+		return dao.findByParentId(parentId);
+	}
 	
 }
