d51412f9a795b7c457d4ec61183b43de94e3e6d0
tsingheng <tsingheng@163.com>
2015-04-14 15:35:56 +0800

    deploy script

--- deploy-openshift.sh
+++ deploy-openshift.sh
@@ -8,14 +8,20 @@ echo "update eshop"
 git fetch
 git rebase origin/store-b2b
 
+echo "mvn clean"
+mvn clean
+
 echo "mvn install"
 mvn install
+
+echo "rm ~/jbossews/webapps/ROOT.war"
+cp rm ~/jbossews/webapps/ROOT.war
 
 echo "cp shop/target/shop-*.war ~/jbossews/webapps/ROOT.war"
 cp cp shop/target/shop-*.war ~/jbossews/webapps/ROOT.war
 
-echo "ctl_app stop"
-ctl_app stop
+#echo "ctl_app stop"
+#ctl_app stop
 
 #echo "ps auxww | grep tomcat| awk '$NF == "start" { system("kill -9 " $2)}'"
 #ps auxww | grep "tomcat"| awk '$NF == "start" { system("kill -9 " $2)}'
@@ -26,8 +32,8 @@ ctl_app stop
 echo "rm -rf ~/jbossews/work/Catalina/localhost/ROOT"
 rm -rf ~/jbossews/work/Catalina/localhost/ROOT
 
-echo "ctl_app start"
-ctl_app start
+#echo "ctl_app start"
+#ctl_app start
 
-echo "tail -f ~/app-root/logs/jbossews.log"
-tail -f ~/app-root/logs/jbossews.log
+#echo "tail -f ~/app-root/logs/jbossews.log"
+#tail -f ~/app-root/logs/jbossews.log
